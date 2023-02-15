6e57b7edb3fcc6e290ac78fe53215aa6f01a9439
Franciscob1218 <Francsicob1218@gmail.com>
2023-02-14 20:36:11 -0600

    changed the t.sh to export more information

--- Lab01/t.sh
+++ Lab01/t.sh
@@ -1,9 +1,12 @@
 gcc ./sh.c -o "./shell.out"
+echo "# Notice: sh.c built into shell.out"
+
 gcc ./whatif.c -o "./whatif.out"
+echo "# Notice: whatif.c built into whatif.out"
 
-chmod +x ./shell.out
-chmod +x ./whatif.out
+chmod +wrx ./shell.out
+chmod +wrx ./whatif.out
+echo "# Notice: Changed the permissions of both built files"
 
+echo "# Notice: Running Shell Now!"
 ./shell.out
-
-
