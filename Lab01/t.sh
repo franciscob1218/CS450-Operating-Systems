5cc5e90a8d460480e166945b040f367c7a80d328
Franciscob1218 <Francsicob1218@gmail.com>
2023-02-14 22:45:36 -0600

    changed comments in t.sh

--- Lab01/t.sh
+++ Lab01/t.sh
@@ -1,12 +1,12 @@
-gcc ./sh.c -o "./shell.out"
 echo "# Notice: sh.c built into shell.out"
+gcc ./sh.c -o "./shell.out"
 
-gcc ./whatif.c -o "./whatif.out"
 echo "# Notice: whatif.c built into whatif.out"
+gcc ./whatif.c -o "./whatif.out"
 
+echo "# Notice: Changed the permissions of both built files"
 chmod +wrx ./shell.out
 chmod +wrx ./whatif.out
-echo "# Notice: Changed the permissions of both built files"
 
 echo "# Notice: Running Shell Now!"
 ./shell.out
